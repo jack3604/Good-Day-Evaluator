aaa893ecc0df463806ffcb213aa68e54a5edb2d9
FSW <akeenan360@gmail.com>
2024-10-16 17:59:10 -0400

    added getters and setters and started IsTodayGood()

--- GoodDay.java
+++ GoodDay.java
@@ -4,11 +4,14 @@
  *
  * @author Adisson Buchanan
  */
+import java.util.Random;
+
 public class GoodDay {
     private int Temperature;
     private double Humidity;
     private boolean IsHomeworkDone;
     private String DinnerMeal;
+    private Random rand = new Random();
 
     GoodDay() {
         Temperature = 0;
@@ -24,11 +27,35 @@ public class GoodDay {
         DinnerMeal = dinnerMeal;
     }
 
-    public boolean IsTodayGood() {
+    public int GetTemperature(int temperature) { return this.Temperature; }
+
+    public void SetTemperature(int temperature) { this.Temperature = temperature; }
+
+    public double GetHumidity(int humidity) { return this.Humidity; }
+
+    public void SetHumidity(int humidity) { this.Humidity = humidity; }
+
+    public boolean IsHomeworkDone() { return this.IsHomeworkDone; }
+
+    public void SetIsHomework(boolean homework) { this.IsHomeworkDone = homework; }
+
+    public String GetDinnerMeal() { return this.DinnerMeal; }
+
+    public void SetDinnerMeal(String dinnerMeal) { this.DinnerMeal = dinnerMeal; }
+
+    private boolean CoinFlip() { return rand.nextBoolean(); }
+
+    public boolean IsWeatherGood() {
         return true;
     }
 
-    public boolean IsWeatherGood() {
+    public boolean IsTodayGood() {
+        if (!IsWeatherGood() || !IsHomeworkDone()) {
+            if (CoinFlip() == true) {
+                return false;
+            }
+        }
+
         return true;
     }
 
--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Random r = new Random();
-        System.out.println(r.nextInt(100));
+        Random random = new Random();
+        System.out.println(random.nextInt(100));
     }
 }
