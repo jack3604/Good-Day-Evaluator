0a0d6597a62b8c875b0490b5c886b6c6744c807c
Adisson Buchanan <akeenan360@gmail.com>
2024-10-23 16:45:07 -0400

    Final Commit

--- GoodDay.java
+++ GoodDay.java
@@ -15,7 +15,7 @@ public class GoodDay {
 
     GoodDay() {
         Temperature = 0;
-        Humidity = 0;
+        Humidity = 0.0;
         IsHomeworkDone = false;
         DinnerMeal = "";
     }
@@ -27,11 +27,11 @@ public class GoodDay {
         DinnerMeal = dinnerMeal;
     }
 
-    public int GetTemperature(int temperature) { return this.Temperature; }
+    public int GetTemperature() { return this.Temperature; }
 
     public void SetTemperature(int temperature) { this.Temperature = temperature; }
 
-    public double GetHumidity(int humidity) { return this.Humidity; }
+    public double GetHumidity() { return this.Humidity; }
 
     public void SetHumidity(int humidity) { this.Humidity = humidity; }
 
@@ -46,21 +46,52 @@ public class GoodDay {
     private boolean CoinFlip() { return rand.nextBoolean(); }
 
     public boolean IsWeatherGood() {
-        return true;
+        // If the humidity is between 70 and 80 then the weather is good.
+        if (this.Humidity <= 80 && this.Humidity >= 70) {
+            // If the temperature is between 70 and 80 then the weather is good.
+            // Used switch case just to demonstrate for the assignment.
+            switch (this.Temperature) {
+                case 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        return false;
     }
 
     public boolean IsTodayGood() {
+        // If the weather isn't good but homework is done return true.
+        // If the weather is good but homework isn't done, still return true.
+        // If both are false, flip a coin to see if the day is good.
         if (!IsWeatherGood() || !IsHomeworkDone()) {
-            if (CoinFlip() == true) {
+            if (CoinFlip()) {
+                return false;
+            }
+            // If the coin flip succeeds and dinner isn't some kind of pizza return false.
+            else if (!this.DinnerMeal.toLowerCase().contains("pizza")) {
                 return false;
             }
         }
 
+        // If it's not a bad day, it's a good day.
         return true;
     }
 
     public String ToString() {
+        // Warn about the default constructor being used and the values not being updated.
+        if (this.Temperature == 0 && this.Humidity == 0.0 && !this.IsHomeworkDone && this.DinnerMeal.isEmpty()) {
+            return "Default constructor was used and not updated.";
+        }
+
         String output = "";
+
+        output += "Temperature: " + this.Temperature + "\n";
+        output += "Humidity: " + this.Humidity + "\n";
+        output += "IsHomeworkDone: " + this.IsHomeworkDone + "\n";
+        output += "DinnerMeal: " + this.DinnerMeal + "\n";
+        output += "Good day?: " + this.IsTodayGood() + "\n";
 
         return output;
     }
--- Main.java
+++ Main.java
@@ -2,7 +2,28 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Random random = new Random();
-        System.out.println(random.nextInt(100));
+        Random rand = new Random();
+        GoodDay day = new GoodDay();
+
+        System.out.println("Setters and Getters");
+        day.SetTemperature(rand.nextInt(70,80));
+        System.out.println(day.GetTemperature());
+        day.SetHumidity(rand.nextInt(70,80));
+        System.out.println(day.GetHumidity());
+        day.SetIsHomework(rand.nextBoolean());
+        System.out.println(day.IsHomeworkDone());
+        day.SetDinnerMeal("Pepperoni Pizza");
+        System.out.println(day.GetDinnerMeal() + "\n");
+
+        System.out.println(day.ToString());
+
+        // Relies on the coin flip
+        day = new GoodDay(60, 60.0, true, "pizza");
+        System.out.println(day.ToString());
+
+        // Always returns false
+        day = new GoodDay(90, 75.0, true, "not p1zza");
+        System.out.println(day.ToString());
+
     }
 }
