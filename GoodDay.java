81a42388698dfeb74a2c7cb610923a8625521491
Adisson Buchanan <akeenan360@gmail.com>
2024-10-23 17:04:34 -0400

    Final Commit. with javadoc

--- GoodDay.java
+++ GoodDay.java
@@ -1,11 +1,11 @@
 /**
  * GoodDay class for Module 5
- *
+ * This GoodDay class is used to determine whether it has been a good day or not using
+ * the temperature, humidity, what's for dinner and whether homework is done or not.
  *
  * @author Adisson Buchanan
  */
 import java.util.Random;
-
 public class GoodDay {
     private int Temperature;
     private double Humidity;
@@ -13,6 +13,10 @@ public class GoodDay {
     private String DinnerMeal;
     private Random rand = new Random();
 
+    /**
+     * Default contructor for the GoodDay object
+     * Sets all properties to 0, false or empty strings.
+     */
     GoodDay() {
         Temperature = 0;
         Humidity = 0.0;
@@ -20,6 +24,14 @@ public class GoodDay {
         DinnerMeal = "";
     }
 
+    /**
+     * Parameterized constructor for the GoodDay object
+     *
+     * @param temperature   The temperature for the day.
+     * @param humidity      The humidity for the day.
+     * @param isHomework    True if homework is done, false if not.
+     * @param dinnerMeal    Dinner planned for that day.
+     */
     GoodDay(int temperature, double humidity, boolean isHomework, String dinnerMeal) {
         Temperature = temperature;
         Humidity = humidity;
@@ -27,24 +39,64 @@ public class GoodDay {
         DinnerMeal = dinnerMeal;
     }
 
+    /**
+     * Gets the Temperature for the day.
+     * @return The temperature for the day.
+     */
     public int GetTemperature() { return this.Temperature; }
 
+    /**
+     * Sets the Temperature for the day.
+     * @param temperature   The temperature to be set.
+     */
     public void SetTemperature(int temperature) { this.Temperature = temperature; }
 
+    /**
+     * Gets the Humidity for the day.
+     * @return The humidity for the day.
+     */
     public double GetHumidity() { return this.Humidity; }
 
+    /**
+     * Sets the Humidity for the day.
+     * @param humidity      The humidity to be set.
+     */
     public void SetHumidity(int humidity) { this.Humidity = humidity; }
 
+    /**
+     * Returns a boolean for if homework is done for the day.
+     * @return True if homework is done.
+     */
     public boolean IsHomeworkDone() { return this.IsHomeworkDone; }
 
+    /**
+     * Sets if homework is done for the day.
+     * @param homework      Boolean value for if homework is done for the day.
+     */
     public void SetIsHomework(boolean homework) { this.IsHomeworkDone = homework; }
 
+    /**
+     * Gets the planned meal for dinner.
+     * @return  The planed meal for dinner.
+     */
     public String GetDinnerMeal() { return this.DinnerMeal; }
 
+    /**
+     * Sets the planned meal for dinner.
+     * @param dinnerMeal    The meal you plan to eat for dinner.
+     */
     public void SetDinnerMeal(String dinnerMeal) { this.DinnerMeal = dinnerMeal; }
 
+    /**
+     * Simple coin flip function to return true or false.
+     * @return True or false to simulate a coin flip.
+     */
     private boolean CoinFlip() { return rand.nextBoolean(); }
 
+    /**
+     * Returns a boolean for if the weather is good or not.
+     * @return  A boolean for if the weather is good.
+     */
     public boolean IsWeatherGood() {
         // If the humidity is between 70 and 80 then the weather is good.
         if (this.Humidity <= 80 && this.Humidity >= 70) {
@@ -61,6 +113,10 @@ public class GoodDay {
         return false;
     }
 
+    /**
+     * Returns if the day is good or not.
+     * @return  A boolean for if the day is good.
+     */
     public boolean IsTodayGood() {
         // If the weather isn't good but homework is done return true.
         // If the weather is good but homework isn't done, still return true.
@@ -79,6 +135,10 @@ public class GoodDay {
         return true;
     }
 
+    /**
+     * Returns a string to be outputed with a summary of the object.
+     * @return An output string with a summary of the object
+     */
     public String ToString() {
         // Warn about the default constructor being used and the values not being updated.
         if (this.Temperature == 0 && this.Humidity == 0.0 && !this.IsHomeworkDone && this.DinnerMeal.isEmpty()) {
